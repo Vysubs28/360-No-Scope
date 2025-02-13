901ed4db7da964323d5df5abe4a05c60b1cbd387
shuja-waraich-03 <suwaraich@knox.edu>
2025-02-13 12:09:03 -0600

    Fixed Head Issue

--- Level/world.gd
+++ Level/world.gd
@@ -19,15 +19,6 @@ func _on_mc_shoot(pos):
 	$Bullets.add_child(bullet)
 	bullet.position = pos + Vector2(8, -170)
 	
-<<<<<<< HEAD
-=======
-<<<<<<< HEAD
-<<<<<<< Updated upstream
-=======
-=======
->>>>>>> Dev2
->>>>>>> Vyaas
-
 
 func _on_enemy_1_enemy_died():
 	print("We got to the signal")
@@ -36,22 +27,9 @@ func _on_enemy_1_enemy_died():
 		player.fly()
 	else:
 		print("found an error")
-<<<<<<< HEAD
-=======
-<<<<<<< Updated upstream
-<<<<<<< HEAD
-=======
->>>>>>> Stashed changes
 		
 func _on_exit_pressed():
 	get_tree().change_scene_to_file("res://Menu/main_menu.tscn")
 	
 func _on_restart_pressed():
 	get_tree().reload_current_scene()
-<<<<<<< Updated upstream
->>>>>>> Stashed changes
-=======
->>>>>>> Dev2
-=======
->>>>>>> Stashed changes
->>>>>>> Vyaas
