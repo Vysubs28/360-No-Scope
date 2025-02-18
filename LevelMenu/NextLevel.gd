f3186ecdc74c1e212c556e24f66f37e62e7c3a91
Vyaas Subramanian <128542233+Vysubs28@users.noreply.github.com>
2025-02-18 12:13:38 -0600

    next level carousel

--- LevelMenu/NextLevel.gd
+++ LevelMenu/NextLevel.gd
@@ -1,6 +1,8 @@
 extends Control
 
-
+#NextLevel_1 is first, Level 2,3,4 come after next level screen. Only Level 1 "tutorial"
+# doesnt have a NextLevel tag
+const FILE_BEGIN = "res://LevelMenu/NextLevel_"
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	pass # Replace with function body.
@@ -9,4 +11,7 @@ func _on_exit_pressed():
 	get_tree().change_scene_to_file("res://Menu/main_menu.tscn")
 
 func _on_next_pressed():
-	get_tree().change_scene_to_file("res://Level/Level 2/level2.tscn")
+	var current_scence_file = get_tree().current_scene.scene_file_path
+	var next_level_number = current_scence_file.to_int() + 1
+	var next_level_path = "res://LevelMenu/NextLevel_" + str(next_level_number) + ".tscn"
+	get_tree().change_scene_to_file(next_level_path)
