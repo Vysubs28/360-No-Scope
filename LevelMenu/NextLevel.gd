6913d63535d2c30639eb82ccd92f038e2f4869c5
Vyaas Subramanian <128542233+Vysubs28@users.noreply.github.com>
2025-02-18 11:25:15 -0600

    Victorious screen

---
+++ LevelMenu/NextLevel.gd
@@ -0,0 +1,12 @@
+extends Control
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass # Replace with function body.
+
+func _on_exit_pressed():
+	get_tree().change_scene_to_file("res://Menu/main_menu.tscn")
+
+func _on_next_pressed():
+	get_tree().change_scene_to_file("res://Level/Level 2/level2.tscn")
