30ba5413fe2d31ce5bb6c7c8e71880bf2d95b102
zvshack <zvshack@knox.edu>
2025-02-18 12:11:55 -0600

    Arrow being everywhere

--- Level/world.gd
+++ Level/world.gd
@@ -17,7 +17,7 @@ func _ready():
 func _on_mc_shoot(pos): 
 	var bullet = bullet_scene.instantiate()
 	$Bullets.add_child(bullet)
-	bullet.position = pos + Vector2(8, -170)
+	bullet.position = pos + Vector2(-10, -170)
 
 func _on_enemy_1_enemy_died():
 	print("We got to the signal")
