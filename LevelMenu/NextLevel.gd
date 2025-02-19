f2dc55b634c7345be57871a2c14619ce36103a47
Vyaas Subramanian <128542233+Vysubs28@users.noreply.github.com>
2025-02-18 19:42:51 -0600

    Level Buttons and Menu fixed

--- LevelMenu/NextLevel.gd
+++ LevelMenu/NextLevel.gd
@@ -10,4 +10,5 @@ func _ready():
 func _on_exit_pressed():
 	get_tree().change_scene_to_file("res://Menu/main_menu.tscn")
 
-
+func _on_next_pressed():
+	get_tree().change_scene_to_file("res://Level/Level 2/Level2.tscn")
