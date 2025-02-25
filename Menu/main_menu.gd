4068baa7d2266b842b316700db0d82def5afc552
Vyaas Subramanian <128542233+Vysubs28@users.noreply.github.com>
2025-02-25 11:46:43 -0600

    Tutorial Working

--- Menu/main_menu.gd
+++ Menu/main_menu.gd
@@ -11,7 +11,7 @@ func _process(delta):
 	pass
 
 func _on_start_pressed():
-	get_tree().change_scene_to_file("res://Level/level1.tscn")
+	get_tree().change_scene_to_file("res://Menu/tutorial_menu.tscn")
 
 
 func _on_levels_pressed():
