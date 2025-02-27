e5e77524727ef6e07b3931d2024611514eb8e2ad
Vyaas Subramanian <128542233+Vysubs28@users.noreply.github.com>
2025-02-27 14:52:42 -0600

    fixed level order

--- Level/world.gd
+++ Level/world.gd
@@ -18,17 +18,21 @@ func _on_mc_shoot(pos):
 	$Bullets.add_child(bullet)
 	bullet.position = pos + Vector2(-10, -170)
 
-func _on_enemy_1_enemy_died():
-	print("We got to the signal")
-	var player = get_node("MC")
-	if player:
-		player.fly()
-		get_tree().change_scene_to_file("res://LevelMenu/NextLevel5.tscn")
-	else:
-		print("found an error")
+
+
 		
 func _on_exit_pressed():
 	get_tree().change_scene_to_file("res://Menu/main_menu.tscn")
 	
 func _on_restart_pressed():
 	get_tree().reload_current_scene()
+
+
+func _on_enemy_1_enemy_died():
+	print("We got to the signal")
+	var player = get_node("MC")
+	if player:
+		player.fly()
+		get_tree().change_scene_to_file("res://LevelMenu/NextLevel.tscn")
+	else:
+		print("found an error")
