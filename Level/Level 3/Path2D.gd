f0cb7855a4c0635b52825f59cda34bf44302f62a
Vyaas Subramanian <128542233+Vysubs28@users.noreply.github.com>
2025-03-05 17:41:39 -0600

    trying to get death on orc

--- Level/Level 3/Path2D.gd
+++ Level/Level 3/Path2D.gd
@@ -6,11 +6,19 @@ extends Path2D
 
 @onready var path = $PathFollow2D
 @onready var animation = $AnimationPlayer
+@onready var animated_sprite : AnimatedSprite2D = $AnimatedSprite2D
+@onready var Enemy_Death = $Enemy_Death
+signal enemy_died
+# Get the gravity from the project settings to be synced with RigidBody nodes.
+var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
+var is_dead = false
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	
 	#if not loop:
 		animation.play("Enemy_Walking")
+		if is_dead:
+			killEnemy()
 		#animation.speed_scale = speed_scale
 		#set_process(false)
 
@@ -18,3 +26,14 @@ func _ready():
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
 	path.progress += speed
+
+func killEnemy():
+	if is_dead:
+		return
+	is_dead = true
+	enemy_died.emit()
+	print('alien1 hit')
+	animated_sprite.play("death")
+	Enemy_Death.play()
+	await animated_sprite.animation_finished
+	queue_free()
