6eaf7f09e84956f3129ebd9ba2cb8808335bd123
shuja-waraich-03 <suwaraich@knox.edu>
2025-03-06 11:32:58 -0600

    House Wall

--- Assets/Rockstar Studios/Kill them all!/orc.gd
+++ Assets/Rockstar Studios/Kill them all!/orc.gd
@@ -3,7 +3,7 @@ extends AnimatedSprite2D
 @onready var Enemy_Death = $Enemy_Death
 signal enemy_died
 # Get the gravity from the project settings to be synced with RigidBody nodes.
-var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
+#var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
 var is_dead = false
 
 
