94a6aff7700be0ed1e28dea994a0a9cccdd79e10
zvshack <zvshack@knox.edu>
2025-03-09 15:58:29 -0500

    added enemy getting hit, linked signal

--- Level/Level 3/AnimatableBody2D.gd
+++ Level/Level 3/AnimatableBody2D.gd
@@ -1,29 +1,5 @@
-extends AnimatedSprite2D
-@onready var animated_sprite : AnimatedSprite2D = $AnimatedSprite2D
-@onready var Enemy_Death = $Enemy_Death
-signal enemy_died
-# Get the gravity from the project settings to be synced with RigidBody nodes.
-var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
-var is_dead = false
-
-
-
-func change_direction():
-	self.scale.x *= -1
-	if(is_dead == false):
-		$Timer.start()
-
+extends AnimatableBody2D
 func killEnemy():
-	if is_dead:
-		return
-	is_dead = true
-	enemy_died.emit()
-	print('alien1 hit')
-	animated_sprite.play("death")
-	Enemy_Death.play()
-	await animated_sprite.animation_finished
-	queue_free()
-
-
-func _on_timer_timeout():
-	change_direction()
+	var parent = get_parent()
+	if parent:
+		parent.rkillEnemy()
